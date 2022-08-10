faf1b53494bd0e17a33886f272fde9528f656ab5
Milen_Mladenov <milen_s_mladenov@yahoo.co.uk>
2022-08-10 23:08:42 +0300

    employee creation

--- src/components/EmployeeManagement/ManageAccounts/ManageAccounts.js
+++ src/components/EmployeeManagement/ManageAccounts/ManageAccounts.js
@@ -4,13 +4,6 @@ import styles from "./ManageAccounts.module.css"
 
 export const ManageAccounts = () => {
     const [passwordType, setPassowrdType] = useState("password")
-    const [values, setValues] = useState({
-        fName: "",
-        lName: "",
-        password: "",
-        phone: "",
-        email: "",
-    });
 
     function handlePassType(e) {
         e.preventDefault()
@@ -21,23 +14,24 @@ export const ManageAccounts = () => {
         }
     }
 
-    function onInput(e) {
-        setValues(state => ({
-            ...state,
-            [e.target.name]: e.target.value,
-        }))
-    }
+
 
     function createAccount(e) {
         e.preventDefault();
-        console.log(values);
-        setValues({
-            fName: "",
-            lName: "",
-            password: "",
-            phone: "",
-            email: "",
-        })
+
+        let data = new FormData(e.target)
+
+        const employee = {
+            "Position": data.get("Position"),
+            "fName": data.get("fName"),
+            "lName": data.get("lName"),
+            "password": data.get("password"),
+            "email": data.get("email"),
+            "phone": data.get("phone"),
+            "note": data.get("note")
+        }
+
+        console.log(employee);
     }
 
 
@@ -47,25 +41,43 @@ export const ManageAccounts = () => {
             <div className={styles.employee_form}>
                 <form onSubmit={createAccount} className={styles.create_employee}>
                     <div>
-                        <label htmlFor="fName"> First Name </label>
-                        <input type="text" name="fName" onChange={onInput} value={values.fName} />
-                    </div>
-                    <div>
-                        <label htmlFor="lName"> Last Name </label>
-                        <input type="text" name="lName" onChange={onInput} value={values.lName} />
-                    </div>
-                    <div>
-                        <label htmlFor="password"> Password </label>
-                        <input type={passwordType} name="password" onChange={onInput} value={values.password} />
-                        <button onClick={handlePassType}>show/hide</button>
+                        <label htmlFor="Position"> Position </label>
+                        <select name="Position">
+                            <option value="Servers">Servers</option>
+                            <option value="Bar">Bar</option>
+                            <option value="Kitchen">Kitchen</option>
+                            <option value="Managers">Managers</option>
+                        </select>
                     </div>
                     <div>
                         <label htmlFor="phone"> Phone number </label>
-                        <input type="number" name="phone" onChange={onInput} value={values.phone} />
+                        <input type="number" name="phone" />
+                    </div>
+                    <div>
+                        <label htmlFor="fName"> First Name </label>
+                        <input type="text" name="fName" />
+                    </div>
+                    <div>
+                        <label htmlFor="lName"> Last Name </label>
+                        <input type="text" name="lName" />
+                    </div>
+                    <div>
+                        <label htmlFor="password"> Password </label>
+                        <input type={passwordType} name="password" />
+                        <button onClick={handlePassType}>show/hide</button>
+                    </div>
+                    <div>
+                        <label htmlFor="rePassword"> Repeat Password </label>
+                        <input type={passwordType} name="rePassword" />
+                        <button onClick={handlePassType}>show/hide</button>
                     </div>
                     <div>
                         <label htmlFor="email"> Email </label>
-                        <input type="text" name="email" onChange={onInput} value={values.email} />
+                        <input type="text" name="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="note"> Note </label>
+                        <textarea type="text" name="note" />
                     </div>
                     <button>Create</button>
                 </form>
